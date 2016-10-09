af14aad68fde07e9d749358f0e95caccf6c15557
Chromatk <harrykwon20@gmail.com>
2016-10-09 13:58:29 -0400

    S1 C1

---
+++ Set1/1-hexToBase64.py
@@ -0,0 +1,6 @@
+hexEncodedString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+
+decoded = hexEncodedString.decode("hex").encode("base64")
+
+print(decoded)	
