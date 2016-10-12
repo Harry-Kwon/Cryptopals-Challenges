327d7d36b1fe8d41f507422eb0c9d58a92caebf9
Chromatk <harrykwon20@gmail.com>
2016-10-12 02:15:22 -0400

    cleaned kwonCrypt

--- Set1/kwonCrypt.py
+++ Set1/kwonCrypt.py
@@ -4,7 +4,7 @@ def xor(bx, by):
 	return "".join(chr(ord(x) ^ ord(y)) for x, y in zip(bx, by))
 
 def singleXOR(t, b):
-	return "".join(chr(ord(tx) ^ b) for tx in t)
+	return "".join(chr(ord(tx) ^ ord(b)) for tx in t)
 
 def rateString(px):
 	freqs = {}
@@ -24,41 +24,29 @@ def rateString(px):
 		score += abs(charFreqs.freqs[chr(i)] - freqs[i])
 	return 1.0/score
 
-def decipherSingleXOR(sourceText):
-	cText = sourceText.decode("hex")
-	bestScore = -1
-	bestText = ""
-	for x in reversed(range(0, 256)):
-		pText = singleXOR(cText, x)
-		score = rateString(pText)
-		if(score > bestScore):
-			bestScore = score
-			bestText = pText
-	return(bestText, bestScore)
-
-def findSingleXORKey(sourceText):
+def decryptSingleXOR(sourceText):
 	bestScore = -1
 	bestKey = 0
+	bestPText = ""
 	for x in reversed(range(0, 256)):
 		pText = singleXOR(sourceText, x)
 		score = rateString(pText)
 		if(score > bestScore):
 			bestScore = score
 			bestKey = x
-	return chr(bestKey)
+			bestPText = pText
+	return bestPText, chr(bestKey), bestScore
 
-def findSingleXORFile(fileName, delim):
-	sourceFile = open(fileName, 'r')
-	sources = str(sourceFile.read()).split(delim)
+def findSingleXOR(sources):
 	bestScore = -1
 	bestSource = ""
 	bestText = ""
 
 	for s in sources:
-		text, score = decipherSingleXOR(s) 
+		text, key, score = decryptSingleXOR(s) 
 		if score > bestScore:
-			bestScore = score
 			bestText = text
+			bestScore = score
 			bestSource = s
 
 	return bestSource, bestText, bestScore
@@ -70,10 +58,3 @@ def repeatXOR(plainText, key):
 		ciphertext += chr(ord(plainText[i]) ^ ord(key[i%len(key)]))
 	return ciphertext
 
-def repeatXORFile(key, inFile, outFile):	
-	f = open(inFile, "r")
-	t = str(f.read())
-	cText = repeatXOR(t, key)
-
-	o = open(outFile, "w+")
-	o.write(cText)
